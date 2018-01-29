66c0a0b32d8a3477e413706b7fe6254e44eb16b0
anlaneg <anlaneg@example.com>
2018-01-29 16:45:22 +0800

    remove sudo

--- Setup.sh
+++ Setup.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-sudo ./tools/vnf_build.sh -s -d=17.05
+if [ ! `id -u` -eq  0 ];
+then
+    echo "Checking for user permission.. Password-less sudo user must run this script"
+    exit 1
+fi;
+./tools/vnf_build.sh -s -d=17.05
